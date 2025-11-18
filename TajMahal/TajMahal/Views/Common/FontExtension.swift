7a60a48c136edb84a3c853c65a9dad5d6e18ab3e
Mathieu ARRIO <minimat26git@arrio.fr>
2025-11-18 08:30:59 +0100

    feat(assets): add custom colors, fonts and symbols

---
+++ TajMahal/TajMahal/Views/Common/FontExtension.swift
@@ -0,0 +1,17 @@
+//
+//  FontExtension.swift
+//  TajMahal
+//
+//  Created by Mathieu ARRIO on 06/11/2025.
+//
+
+import SwiftUI
+
+extension Font {
+    static let FontBold14 = Font.custom("PlusJakartaSans-Bold", size: 14)
+    static let FontBold16 = Font.custom("PlusJakartaSans-Bold", size: 16)
+    static let FontBold18 = Font.custom("PlusJakartaSans-Bold", size: 18)
+    static let FontSemiBold12 = Font.custom("PlusJakartaSans-SemiBold", size: 12)
+    static let FontSemiBold14 = Font.custom("PlusJakartaSans-SemiBold", size: 14)
+    static let FontRegular12 = Font.custom("PlusJakartaSans-Regular", size: 12)
+}
