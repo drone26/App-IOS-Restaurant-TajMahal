d363a99cd38a6e657eca90f0792552d090e08bd3
Mathieu ARRIO <minimat26git@arrio.fr>
2025-11-18 10:05:21 +0100

    feat(views): add Menu view

--- TajMahal/TajMahal/Views/Menu/MenuView.swift
+++ TajMahal/TajMahal/Views/Menu/MenuView.swift
@@ -13,8 +13,79 @@ struct MenuView: View {
     let viewModel: ViewModel = ViewModel()
     
     var body: some View {
-        List {
-            // À completer
+        NavigationStack {
+            ZStack {
+                
+                Color(.customGrayF1F1F1)
+                    .ignoresSafeArea()
+                
+                List {
+                    Section {
+                        
+                        NavigationLink {
+                            Text("Vue des plats")
+                        } label: {
+                            DishShortView(dish: viewModel.apetizerArray[0])
+                                .background(.white)
+                        }
+                        .listRowInsets(EdgeInsets(top: 0, leading: 20, bottom: 10, trailing: 20))
+                        NavigationLink {
+                            Text("Vue des plats")
+                        } label: {
+                            DishShortView(dish: viewModel.apetizerArray[1])
+                                .background(.white)
+                        }
+                        .listRowInsets(EdgeInsets(top: 0, leading: 20, bottom: 10, trailing: 20))
+                    } header: {
+                        Text("Entrées")
+                            .font(.FontBold14)
+                            .foregroundStyle(Color(.customGray666666))
+                            .background(.customGrayF1F1F1)
+                    }
+                    .background(.white)
+                    .listRowBackground(RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.customGrayF1F1F1)
+                    )
+                    .listRowSeparator(.hidden)
+                    .listRowSpacing(0)
+                    .listSectionSpacing(0)
+                    .cornerRadius(10)
+                    
+                    Section {
+                        NavigationLink {
+                            Text("Vue des plats")
+                        } label: {
+                            DishShortView(dish: viewModel.mainCourseArray[0])
+                                .background(.white)
+                        }
+                        .listRowInsets(EdgeInsets(top: 0, leading: 20, bottom: 10, trailing: 20))
+                        NavigationLink {
+                            Text("Vue des plats")
+                        } label: {
+                            DishShortView(dish: viewModel.mainCourseArray[1])
+                                .background(.white)
+                        }
+                        .listRowInsets(EdgeInsets(top: 0, leading: 20, bottom: 10, trailing: 20))
+                    } header: {
+                        Text("Plats principaux")
+                            .font(.FontBold14)
+                            .foregroundStyle(Color(.customGray666666))
+                            .background(.customGrayF1F1F1)
+                    }
+                    .background(.white)
+                    .listRowBackground(RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.customGrayF1F1F1)
+                    )
+                    .listRowSeparator(.hidden)
+                    .listRowSpacing(0)
+                    .listSectionSpacing(0)
+                    .cornerRadius(10)
+                }
+                .listStyle(.plain)
+                
+            }
+            .navigationTitle("Menu")
+            .font(.FontBold18)
         }
     }
 }
